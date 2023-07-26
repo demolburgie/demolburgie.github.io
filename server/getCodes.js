4e1de169038a995ff93a9f25ec8148a26063976a
davpouck <Dante.VanPoucke@UGent.be>
2023-07-27 00:08:27 +0200

    f

--- server/getCodes.js
+++ server/getCodes.js
@@ -69,14 +69,160 @@ class Codes {
 
 let codes = new Codes()
 
-codes.addCode("test", "room1", 1, 1, "t")
-codes.addCode("test2", "room1", -1, 1)
-codes.addCode("som112", "room1", 5, 5, "som")
-codes.addCode("som112", "room2", 1, 1, "som")
-codes.addCode("som112", "room3", 1, 1, "som")
-codes.addCode("6734912", "room1", -1, 3) // audio
-codes.addCode("6734912", "room2", -1, 3) // audio
-codes.addCode("6734912", "room3", -1, 3) // audio
+codes.addCode("ik_ben_de_mol", "room1", -1, 1)
+codes.addCode("ik_ben_de_mol", "room2", -1, 1)
+codes.addCode("ik_ben_de_mol", "room3", -1, 1)
+
+codes.addCode("sudoku_7132", "room1", 3, 3, "sudoku")
+codes.addCode("sudoku_7132", "room2", 3, 3, "sudoku")
+codes.addCode("sudoku_7132", "room3", 3, 3, "sudoku")
+
+codes.addCode("octadoku_572", "room1", 3, 5, "octadoku")
+codes.addCode("octadoku_1012", "room2", 3, 5, "octadoku")
+codes.addCode("octadoku_316", "room3", 3, 5, "octadoku")
+
+// TODO: 17*blauw + 26*geel + 35*groen + 59*rood
+codes.addCode("", "room1", -1, 5)
+codes.addCode("", "room2", -1, 5)
+codes.addCode("", "room3", -1, 5)
+
+codes.addCode("winnen", "room1", -1, 1)
+codes.addCode("winnen", "room2", -1, 1)
+codes.addCode("winnen", "room3", -1, 1)
+codes.addCode("de_mol", "room1", -1, 1)
+codes.addCode("de_mol", "room2", -1, 1)
+codes.addCode("de_mol", "room3", -1, 1)
+codes.addCode("escape_room", "room1", -1, 1)
+codes.addCode("escape_room", "room2", -1, 1)
+codes.addCode("escape_room", "room3", -1, 1)
+codes.addCode("spannend", "room1", -1, 1)
+codes.addCode("spannend", "room2", -1, 1)
+codes.addCode("spannend", "room3", -1, 1)
+codes.addCode("burgies", "room1", -1, 1)
+codes.addCode("burgies", "room2", -1, 1)
+codes.addCode("burgies", "room3", -1, 1)
+
+codes.addCode("usb_is_geen_bus", "room1", -1, 2)
+codes.addCode("usb_is_geen_bus", "room2", -1, 2)
+codes.addCode("usb_is_geen_bus", "room3", -1, 2)
+
+codes.addCode("kamer_b", "room1", 1, 3, "kamer")
+codes.addCode("kamer_b", "room2", 1, 3, "kamer")
+codes.addCode("kamer_b", "room3", 1, 3, "kamer")
+
+codes.addCode("adonis", "room1", -1, 1)
+codes.addCode("adonis", "room2", -1, 1)
+codes.addCode("adonis", "room3", -1, 1)
+codes.addCode("adios", "room1", -1, 1)
+codes.addCode("adios", "room2", -1, 1)
+codes.addCode("adios", "room3", -1, 1)
+codes.addCode("insop", "room1", -1, 1)
+codes.addCode("insop", "room2", -1, 1)
+codes.addCode("insop", "room3", -1, 1)
+codes.addCode("piano", "room1", -1, 1)
+codes.addCode("piano", "room2", -1, 1)
+codes.addCode("piano", "room3", -1, 1)
+codes.addCode("pinas", "room1", -1, 1)
+codes.addCode("pinas", "room2", -1, 1)
+codes.addCode("pinas", "room3", -1, 1)
+codes.addCode("pinda", "room1", -1, 1)
+codes.addCode("pinda", "room2", -1, 1)
+codes.addCode("pinda", "room3", -1, 1)
+codes.addCode("pions", "room1", -1, 1)
+codes.addCode("pions", "room2", -1, 1)
+codes.addCode("pions", "room3", -1, 1)
+codes.addCode("spion", "room1", -1, 1)
+codes.addCode("spion", "room2", -1, 1)
+codes.addCode("spion", "room3", -1, 1)
+codes.addCode("podia", "room1", -1, 1)
+codes.addCode("podia", "room2", -1, 1)
+codes.addCode("podia", "room3", -1, 1)
+
+// TODO
+codes.addCode("cola_", "room1", 1, 3, "cola")
+codes.addCode("cola_", "room2", 1, 3, "cola")
+codes.addCode("cola_", "room3", 1, 3, "cola")
+ 
+codes.addCode("hey", "room1", -1, 2)
+codes.addCode("hey", "room2", -1, 2)
+codes.addCode("hey", "room3", -1, 2)
+
+codes.addCode("schaak_c3e5", "room1", 1, 3, "schaak")
+codes.addCode("schaak_f5h6", "room2", 1, 3, "schaak")
+codes.addCode("schaak_d6g6", "room3", 1, 3, "schaak")
+
+codes.addCode("jungle_bc9w", "room1", 2, 5, "jungle")
+codes.addCode("jungle_48de", "room2", 2, 5, "jungle")
+codes.addCode("jungle_rf4w", "room3", 2, 5, "jungle")
+
+codes.addCode("driehoek", "room1", -1, 5)
+codes.addCode("driehoek", "room2", -1, 5)
+codes.addCode("driehoek", "room3", -1, 5)
+
+codes.addCode("raadsel_1987", "room1", 1, 5, "raadsel")
+codes.addCode("raadsel_1987", "room2", 1, 5, "raadsel")
+codes.addCode("raadsel_1987", "room3", 1, 5, "raadsel")
+
+codes.addCode("wortel_28908", "room1", -1, 5)
+codes.addCode("wortel_28908", "room2", -1, 5)
+codes.addCode("wortel_28908", "room3", -1, 5)
+
+codes.addCode("vlaemsche_kermis", "room1", -1, 5)
+codes.addCode("vlaemsche_kermis", "room2", -1, 5)
+codes.addCode("vlaemsche_kermis", "room3", -1, 5)
+
+codes.addCode("6734912", "room1", -1, 5)
+codes.addCode("6734912", "room2", -1, 5)
+codes.addCode("6734912", "room3", -1, 5)
+
+codes.addCode("gaten", "room1", -1, 3)
+codes.addCode("nkbuhwlie", "room1", -1, 3)
+codes.addCode("gaten", "room2", -1, 3)
+codes.addCode("ubhztlefg", "room2", -1, 3)
+codes.addCode("gaten", "room3", -1, 3)
+codes.addCode("xvbheqwag", "room3", -1, 3)
+
+
+codes.addCode("olympiade_b", "room1", 1, 3, "olympiade")
+codes.addCode("olympiade_e", "room2", 1, 3, "olympiade")
+codes.addCode("olympiade_b", "room3", 1, 3, "olympiade")
+
+codes.addCode("binair_1011001010", "room1", -1, 3)
+codes.addCode("binair_1011001010", "room2", -1, 3)
+codes.addCode("binair_1011001010", "room3", -1, 3)
+
+codes.addCode("nonogram_donut", "room1", -1, 3)
+codes.addCode("nonogram_donut", "room2", -1, 3)
+codes.addCode("nonogram_donut", "room3", -1, 3)
+
+codes.addCode("samuel", "room1", -1, 3)
+codes.addCode("samuel", "room2", -1, 3)
+codes.addCode("samuel", "room3", -1, 3)
+
+codes.addCode("symmetrie", "room1", -1, 2)
+codes.addCode("symmetrie", "room2", -1, 2)
+codes.addCode("symmetrie", "room3", -1, 2)
+
+codes.addCode("pi24nk", "room1", -1, -5)
+codes.addCode("pi24nk", "room2", -1, -5)
+codes.addCode("p45_0p", "room1", -1, -5)
+codes.addCode("p45_0p", "room3", -1, -5)
+codes.addCode("nn1nu5", "room2", -1, -5)
+codes.addCode("nn1nu5", "room3", -1, -5)
+
+codes.addCode("pg5t_de7k", "room2", -1, 2)
+codes.addCode("pg5t_de7k", "room3", -1, 2)
+codes.addCode("sdg1_de7k", "room1", -1, 2)
+codes.addCode("sdg1_de7k", "room3", -1, 2)
+codes.addCode("sdg1_pg5t", "room1", -1, 2)
+codes.addCode("sdg1_pg5t", "room2", -1, 2)
+
+
+codes.addCode("74kp9srtqv", "room1", -1, 3)
+codes.addCode("74kp9srtqv", "room2", -1, 3)
+codes.addCode("74kp9srtqv", "room3", -1, 3)
+
+
 
 module.exports = function() {return codes;}
 
